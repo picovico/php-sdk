6386e8d6c6da8b10c1cd4c5e5dd079f4bee50043
Dhruba Adhikari <dhrubaster@gmail.com>
2012-04-16 02:52:40 +0545

    the Client libraries release pack

--- examples/config.example.php
+++ examples/config.example.php
@@ -1,11 +1,8 @@
 <?php
 
 // my access token
-$PV_config["access_token"] = "2|iOcYxmxvhS6aqBaKQvUCtX9kF3VWU5ju2D1xZnStPJU";
+$PV_config["access_token"] = "";
 
 // some of my videos
 $PV_config["video_tokens"] = array();
 
-$PV_config["video_tokens"][] = "511df1cf315f4f359375bbf4621ce3ae";
-$PV_config["video_tokens"][] = "1358228e05df4ff88d223920f3bf6601";
-$PV_config["video_tokens"][] = "6dc9cf6ca86449e69d1e9ea30e6906e2";
